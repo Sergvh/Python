d94e3596a54d52c3f974cf1f6f4791a417d44b87
”Serhii <sergvh@gmail.com>
2018-10-07 19:31:21 +0300

    Minimum hexagon drawing

---
+++ hw1/min_hexagon.py
@@ -0,0 +1,28 @@
+import turtle
+import time
+
+turtle.pu()
+turtle.setpos(-200,0)
+turtle.pd()
+
+turtle.pencolor("brown")
+turtle.width(3)
+
+turtle.right(60)
+turtle.forward(200)
+time.sleep(1.5)
+turtle.left(60)
+turtle.forward(200)
+time.sleep(1.5)
+turtle.left(60)
+turtle.forward(200)
+time.sleep(1.5)
+turtle.left(60)
+turtle.forward(200)
+time.sleep(1.5)
+turtle.left(60)
+turtle.forward(200)
+time.sleep(1.5)
+turtle.left(60)
+turtle.forward(200)
+time.sleep(3)
