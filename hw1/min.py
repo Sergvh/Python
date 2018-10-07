8ba9d7cea4ad063f284d8cfeb25b11ba781f91a5
”Serhii <sergvh@gmail.com>
2018-10-07 10:06:08 +0300

    Minimum Name enter and The average of 3 numbers

---
+++ hw1/min.py
@@ -0,0 +1,9 @@
+name = input("What is Your name?")
+print("\n Hello, "+name+"!")
+
+#The average
+
+a = int(input("Enter A number please:"))
+b = int(input("Enter B number please:"))
+c = int(input("Enter C number please:"))
+print("The Average is - "+str((a+b+c)/3))
