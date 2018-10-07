7bd3c2b20b60573ed1e645e1948d99970b4bcfdc
”Serhii <sergvh@gmail.com>
2018-10-07 18:24:38 +0300

    Minimal square drawing

---
+++ hw1/min_square.py
@@ -0,0 +1,14 @@
+import turtle
+import time
+
+turtle.forward(300)
+time.sleep(1.5)
+turtle.left(90)
+turtle.forward(300)
+time.sleep(1.5)
+turtle.left(90)
+turtle.forward(300)
+time.sleep(1.5)
+turtle.left(90)
+turtle.forward(300)
+time.sleep(1.5)
