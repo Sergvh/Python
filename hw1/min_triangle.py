1dc437586e57f721f9ccc2c2c19ecb36e9920bd2
”Serhii <sergvh@gmail.com>
2018-10-07 19:32:43 +0300

    Minimum square drawing

--- hw1/min_triangle.py
+++ hw1/min_triangle.py
@@ -1,6 +1,14 @@
 import turtle
 import time
 
+turtle.pu()
+turtle.setpos(-200,0)
+turtle.pd()
+
+turtle.pencolor("blue")
+turtle.width(3)
+
+
 turtle.forward(300)
 time.sleep(1.5)
 turtle.left(120)
