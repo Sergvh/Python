e578c2a10cb9963a78a017901a8913eb5b3026e9
”Serhii <sergvh@gmail.com>
2018-10-07 19:32:22 +0300

    Minimum pentagon drawing

--- hw1/min_pentagon.py
+++ hw1/min_pentagon.py
@@ -1,6 +1,13 @@
 import turtle
 import time
 
+turtle.pu()
+turtle.setpos(-200,0)
+turtle.pd()
+
+turtle.pencolor("yellow")
+turtle.width(3)
+
 turtle.right(72)
 turtle.forward(200)
 time.sleep(1.5)
