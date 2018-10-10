3f59e62c3ee563437e203ae2fe84b90afe495fd4
”Serhii <sergvh@gmail.com>
2018-10-10 21:42:18 +0300

    bubble sort

--- hw2/bubble_Sort.py
+++ hw2/bubble_Sort.py
@@ -2,16 +2,15 @@ import random
 
 count = int(input("Enter amount of list elements:"))
 
-a = [random.randint(1,200) for i in range(1, count)]
+a = [random.randint(1,200) for i in range(0, count)]
+
 print("generated list:"+str(a)+"\n \n")
 
-for i in range(0,count-1, 1):
-    for j in range(0,count-2, 1):
+for i in range(0,count, 1):
+    for j in range(0,count-1, 1):
         if (a[j]>a[j+1]):
             tmp = a[j]
             a[j] = a[j+1]
             a[j+1] = tmp
-        j +=1
-    i +=1
 
 print("sorted list:"+str(a))
