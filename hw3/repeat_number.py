3811415a3b128980a180ab27c6c61b5fb130041b
”Serhii <sergvh@gmail.com>
2018-10-15 20:50:12 +0300

    repeat search

---
+++ hw3/repeat_number.py
@@ -0,0 +1,19 @@
+import random
+
+count = int(input("Enter amount of list elements more then 15:"))
+
+a = [random.randint(1,10) for i in range(0, count)]
+
+print("generated list:"+str(a)+"\n \n")
+flag = 0
+for i in range(0,count-1, 1):
+    if flag == 0:
+        for j in range(i+1, count, 1):
+            if a[i] == a[j]:
+                print("match found:" + str(a[j]))
+                flag = 1
+            break
+    else:
+        break
+
+
