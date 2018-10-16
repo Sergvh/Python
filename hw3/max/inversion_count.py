3915580d266b2bcacadd95a493357e47a8752c08
”Serhii <sergvh@gmail.com>
2018-10-16 20:42:55 +0300

    inversions count

---
+++ hw3/max/inversion_count.py
@@ -0,0 +1,23 @@
+import random
+
+count = int(input("Enter amount of list elements:"))
+
+a = [random.randint(1, 100) for i in range(0, count)]
+
+print("generated list:" + str(a) + "\n \n")
+
+inversions = 0
+
+for i in range(0, count, 1):
+    tmp = a[i]
+    j = i - 1
+    while j >= 0:
+        if tmp < a[j]:
+            a[j + 1], a[j] = a[j], tmp
+            j -= 1
+            inversions +=1
+        else:
+            break
+
+print("sorted list: " + str(a)+"\n")
+print("Inversions count: "+str(inversions))
